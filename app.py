28c8daffe5eebfab31a36c162e93e57779904fea
gil-pyt <gil1999sp@gmail.com>
2024-09-20 21:41:11 -0300

    adding doc

--- app.py
+++ app.py
@@ -3,8 +3,32 @@ import streamlit as st
 # Configuração da página
 st.set_page_config(
     page_title="Calculadora",
-    page_icon="👋",
+    page_icon="🧮",
 )
 
 # Título da página
 st.title("Calculadora Simples")
+
+# Entrada de dados
+num1 = st.number_input("Digite o primeiro número:", step=1.0)
+num2 = st.number_input("Digite o segundo número:", step=1.0)
+
+# Seleção da operação
+operacao = st.selectbox("Selecione a operação", ("Soma", "Subtração", "Multiplicação", "Divisão"))
+
+# Lógica para calcular o resultado
+if operacao == "Soma":
+    resultado = num1 + num2
+elif operacao == "Subtração":
+    resultado = num1 - num2
+elif operacao == "Multiplicação":
+    resultado = num1 * num2
+elif operacao == "Divisão":
+    if num2 != 0:
+        resultado = num1 / num2
+    else:
+        resultado = "Erro! Divisão por zero."
+
+# Exibição do resultado
+if st.button("Calcular"):
+    st.write(f"Resultado: {resultado}")
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Calculadora Streamlit
+
+![Tela inicial](./home.gif)
+
+    Calculadora em streamlit
+
+# Compilação e execução( Build )
+
+```
+poetry install
+ ./run.sh
+```
+## Bibliotecas
+
+* [streamlit](https://streamlit.io/) - Plataforma para desenvolvimento web em python
+* [poetry](https://python-poetry.org/) - Gerenciador de Dependencia
+
+ 
+
+
